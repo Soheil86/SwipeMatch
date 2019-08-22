1eeddc2e8b4590c07c7f977aac4551f862dc3bc6
Amin <amin.fathi@coverholder.it>
2019-08-22 14:23:39 +0200

    User Model

---
+++ SwipeMatch/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Amin Fathi on 11/3/19.
+//  Copyright © 2018 Amin Fathi. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    // defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
+
+
