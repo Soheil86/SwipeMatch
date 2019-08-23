544f7350f5df8dbe2a2935d13d4d350b4c98e5d3
Soheil <soheilghanbari1365@gmail.com>
2019-08-23 11:50:36 +0200

    solving the problem of vertical and horizontal

---
+++ SwipeMatch/Views/CustomTextField.swift
@@ -0,0 +1,39 @@
+//
+//  CustomTextField.swift
+//  SwipeMatch
+//
+//  Created by Soheil Ghanbari on 8/23/19.
+//  Copyright © 2019 Soheil Ghanbari. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    let height: CGFloat
+    
+    init(padding: CGFloat, height: CGFloat) {
+        self.padding = padding
+        self.height = height
+        super.init(frame: .zero)
+        layer.cornerRadius = height / 2
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: height)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
